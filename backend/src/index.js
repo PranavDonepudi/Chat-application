25b154ef1c61cc0267fcdcb0961ec623f46676a6
Pranav Donepudi <donepudipranav04@gmail.com>
2025-02-11 13:56:48 -0500

    Signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,10 @@ import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
+
 const PORT = process.env.PORT;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
